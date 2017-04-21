860a03b68dae10ec00dce90a3a09b7993947619b
Paul Sanford <sanfordpaul@gmail.com>
2017-04-21 10:40:55 -0500

    assignment 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,30 +2,29 @@ var animatePoints = function() {
 
                  var points = document.getElementsByClassName('point');
 
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+                 function revealPoint(pointIndex) {
+                     points[pointIndex].style.opacity = 1;
+                     points[pointIndex].style.color = "yellow";
+                     points[pointIndex].style.transform = "scaleX(1) translateY(pointIndex)";
+                     points[pointIndex].style.msTransform = "scaleX(1) translateY(pointIndex)";
+                     points[pointIndex].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+                     points[pointIndex].firstElementChild.style.color = "red";
+                     points[pointIndex].firstElementChild.style.transform = "rotate(360deg)";
+                     points[pointIndex].firstElementChild.style.msTransform = "rotate(360deg)";
+                     points[pointIndex].firstElementChild.style.WebkitTransform = "rotate(360deg)";
+                 }
+
+                 var title = document.getElementsByClassName('hero-title');
+
+                 var changeTitleColor = function() {
+                     title[0].style.color = "yellow";
+                     title[0].style.textShadow ="2px 2px 0 black"
                  };
 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
-
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-
-
-                 };
-
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
+                 for(var i = 0; i <= 2; i++){
+                     revealPoint(i);
+                 }
+                 changeTitleColor();
              };
