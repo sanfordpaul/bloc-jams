5b283889a51e42801be5ecba65d3126eea72390e
Paul Sanford <sanfordpaul@gmail.com>
2017-04-21 18:06:50 -0500

    Checkpoint 9 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-                 var points = document.getElementsByClassName('point');
-
+var animatePoints = function(points) {
 
                  function revealPoint(pointIndex) {
                      points[pointIndex].style.opacity = 1;
@@ -28,3 +27,18 @@ var animatePoints = function() {
                  }
                  changeTitleColor();
              };
+ window.onload = function() {
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener('scroll', function(event){
+         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+     });
+ }
