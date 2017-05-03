87579ce2ecd897720c8ad6b29878495b4201ac94
Paul Sanford <sanfordpaul@gmail.com>
2017-05-03 09:40:45 -0500

    Small fix

--- scripts/album.js
+++ scripts/album.js
@@ -106,7 +106,7 @@ var nextSong = function() {
     var lastSongNumber = currentlyPlayingSongNumber;
 
     // Set a new current song
-    setSong(currentSongIndex - 1);
+    setSong(currentSongIndex + 1);
     currentSoundFile.play();
 
     // Update the Player Bar information
